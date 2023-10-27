f26a17a7327bd53c690533bcb94b25882efb1d75
VadimK <yaralavadim@gmail.com>
2023-10-27 15:17:10 +0300

    refaktor add_url

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,5 +1,4 @@
 import os
-from urllib.parse import urlparse
 
 import requests
 from dotenv import load_dotenv
@@ -11,7 +10,6 @@ from flask import (
     flash,
     url_for
 )
-from validators.url import url as validate_url
 
 from page_analyzer.db import (
     get_url,
@@ -23,14 +21,12 @@ from page_analyzer.db import (
     insert_url,
     insert_check
 )
-from page_analyzer.parser import get_tags
+from page_analyzer.parser import get_tags, is_valid_url, normalize_url
 
 load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
 app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
-
-successful_http_responses = [200, 201, 204, 202, 203, 205, 206, 207, 208]
 
 
 @app.route('/')
@@ -40,31 +36,31 @@ def index():
 
 @app.post('/urls')
 def add_url():
-    url = request.form.to_dict()['url']
-    url_is_valid = validate_url(url)
+    url = request.form.get("url")
 
-    if url_is_valid and len(url) < 255:
-        parsed_url = urlparse(url)
-        norm_url = f'{parsed_url.scheme}://{parsed_url.netloc}'
-        url_in_bd = get_url(norm_url)
-        if url_in_bd is not None:
-            flash('Страница уже существует', 'error')
-            url_id = url_in_bd[0]
-            return redirect(
-                url_for('show_url', id=url_id),
-                code=302
-            )
+    if is_valid_url(url) != True:
+        flash('Некорректный URL')
+        return render_template('index.html'), 422
 
-        insert_url(norm_url)
-        url_id = get_url_id(norm_url)
+    norm_url = normalize_url(url)
+    url_in_bd = get_url(norm_url)
 
-        flash('Страница успешно добавлена', 'success')
+    if url_in_bd is not None:
+        flash('Страница уже существует', 'error')
+        url_id = url_in_bd[0]
         return redirect(
             url_for('show_url', id=url_id),
             code=302
         )
-    flash('Некорректный URL')
-    return render_template('index.html'), 422
+
+    insert_url(norm_url)
+    url_id = get_url_id(norm_url)
+
+    flash('Страница успешно добавлена', 'success')
+    return redirect(
+        url_for('show_url', id=url_id),
+        code=302
+    )
 
 
 @app.get('/urls')
@@ -95,7 +91,7 @@ def check_url(id):
     request = requests.get(url)
     status_code = request.status_code
 
-    if status_code in successful_http_responses:
+    if status_code == 200:
         tags = get_tags(request.text)
         insert_check(id, status_code, *tags)
         flash('Страница успешно проверена', 'checked')
--- page_analyzer/parser.py
+++ page_analyzer/parser.py
@@ -1,4 +1,26 @@
+from urllib.parse import urlparse
+
 from bs4 import BeautifulSoup
+from validators.url import url as check_url
+
+
+def is_valid_url(url):
+    errors = []
+
+    if not check_url(url):
+        errors.append('Error! Url is invalid')
+
+    if len(url) > 255:
+        errors.append('Error! Url length > 255')
+
+    if errors == []:
+        return True
+    return errors
+
+
+def normalize_url(url):
+    parsed_url = urlparse(url)
+    return f'{parsed_url.scheme}://{parsed_url.netloc}'
 
 
 def get_tags(content):
